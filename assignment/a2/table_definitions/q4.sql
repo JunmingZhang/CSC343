0c78082eb5a4e05e01ad58b8b4e2980c22943572
JunmingZhang <junmingpeter.zhang@mail.utoronto.ca>
2019-07-12 23:21:44 -0400

    part1 q4 solved

--- assignment/a2/table_definitions/q4.sql
+++ assignment/a2/table_definitions/q4.sql
@@ -19,6 +19,34 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here.
 
+-- display countryNanme, cabinetID and startDate
+-- also pick up countryID as coID for joining
+CREATE VIEW NationalCabinet AS
+SELECT country.id AS CoID, country.name AS countryName, cabinet.id AS cabinetID, start_date AS startDate
+FROM cabinet JOIN country ON country.id = cabinet.country_id;
+
+-- add end date to the view
+-- also add partyID for later joining the name of the party with PM
+CREATE VIEW NationalCabinetWithEnd AS
+SELECT countryName, cabinetID, startDate, end_date AS endDate, party_id AS partyID
+FROM NationalCabinet JOIN politician_president ON CoID = politician_president.country_id;
+
+-- find the name of the party fills the PM
+-- also add partyID for later joining the name of the party with PM
+CREATE VIEW PartyName AS
+SELECT party.name AS partyName, party.id AS partyID
+FROM cabinet_party, party
+WHERE cabinet_party.party_id = party.id AND pm = True;
+
+-- join the name of the party with PM
+CREATE VIEW CabinetPmKnown AS
+SELECT countryName, cabinetID, startDate, endDate, partyName AS pmParty
+FROM NationalCabinetWithEnd LEFT JOIN PartyName ON NationalCabinetWithEnd.partyID = PartyName.partyID;
+
+
 
 -- the answer to the query 
-insert into q4 
+insert into q4 (countryName, cabinetID, startDate, endDate, pmParty) (
+        SELECT countryName, cabinetID, startDate, endDate, pmParty
+        FROM CabinetPmKnown
+);
---
+++ assignment/a2/table_definitions/q4_order.sql
@@ -0,0 +1,6 @@
+SET SEARCH_PATH to parlgov;
+
+SELECT *
+FROM q4
+ORDER BY countryName DESC
+         startDate ASC;

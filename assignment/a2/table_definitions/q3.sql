60bb69e81fb9103ce85582fb9e7b065c27cc4d64
JunmingZhang <junmingpeter.zhang@mail.utoronto.ca>
2019-07-14 16:45:27 -0400

    modify comments

--- assignment/a2/table_definitions/q3.sql
+++ assignment/a2/table_definitions/q3.sql
@@ -18,31 +18,37 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here.
 
+-- pick up cabinets in past 20 years
 CREATE VIEW pastTwentyYearCabinets AS
 SELECT id AS CaId, country_id AS CoId
 FROM cabinet
-WHERE (extract(YEAR, start_date) >= 1999) AND (extract(YEAR, start_date) <= 2019);
+WHERE (EXTRACT(YEAR, start_date) >= 1999) AND (EXTRACT(YEAR, start_date) <= 2019);
 
+-- find all combinations of parties in the cabinets
 CREATE VIEW allCombos AS
 SELECT CoId, party.id AS PId, CaId
 FROM pastTwentyYearCabinets JOIN party ON party.country_id = CoId;
 
+-- select info needed for cabinets
 CREATE VIEW inCabinet AS
 SELECT country_id AS CoId, party.id AS PId, cabinet_id AS CaId
 FROM cabinet_party cp JOIN party ON cp.party_id = party.id;
 
+-- find parties not always commited
 CREATE VIEW notAlways AS
 (SELECT *
 FROM allCombos) EXCEPT
 (SELECT *
 FROM inCabinet);
 
+-- find commited parties
 CREATE VIEW commited AS
 (SELECT country_id AS CoId, party.id AS PId
 FROM party) EXCEPT
 (SELECT CoId, PId
 FROM notAlways);
 
+-- find the answer
 CREATE VIEW answer AS
 SELECT co.name AS countryName, p.name AS partyName,
        pf.party_family AS partyFamily, pp.state_market AS stateMarket
@@ -51,12 +57,6 @@ FROM commited JOIN country co ON commited.CoId = co.id
               LEFT JOIN party_family AS pf On commited.PId = pf.party_id
               LEFT JOIN party_position pp ON commited.PId = pp.party_id;
 
-
----CREATE VIEW overTwenty AS
----SELECT countryName, partyName, partyFamily, state_market
----FROM cabinet_party cp JOIN cabinet c ON cp.cabinet_id = c.id
----                      JOIN marketStatus ON cp.party_id = marketStatus.PId
----WHERE start_date > 
 
 -- the answer to the query 
 insert into q3 (countryName, partyName, partyFamily, stateMarket) (
